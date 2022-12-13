6a7bb060300472ea227bd383707e836877db4b4e
moldani <matte.oldani99@gmail.com>
2022-12-13 15:38:08 +0100

    testing tools

---
+++ tools/run.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# setopt localoptions rmstarsilent
+
+rm -f logs/*
+../template_java/cleanup.sh
+../template_java/build.sh
+
+echo "Number of process: "$1
+echo "Number of proposals: "$2
+echo "Max number in proposal: "$3
+echo "Max distinct numbers: "$4
+echo "Execution time: "$5
+
+python3 lattice-performance.py agreement -r ../template_java/run.sh -l logs/ -p $1 -n $2 -v $3 -d $4 -t $5 > /dev/null
+sleep 10
+python3 lattice_validater.py logs logs $1 $2
+wc -l logs/*.output
+
+cat logs/*.stderr
